1d405f02c7a1aa9afc15f2a289cc95e6337aa7c7
Sérgio Félix <sergio98@outlook.pt>
2021-12-17 14:37:55 +0000

    init

--- vendetudo/classes/autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-spl_autoload_register(function ($class_name) {
-	include_once($class_name . '.php');
-});
--- vendetudo/configs/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	"driver" => "mysql",
-	"server" => "127.0.0.1",
-	"port" => "3306",
-	"database" => "vendetudo",
-	"username" => "root",
-	"password" => ""
-);
