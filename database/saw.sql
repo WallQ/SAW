87dceb868796c30527537a22266b1474771693fb
Sérgio Félix <sergio98@outlook.pt>
2021-12-17 13:35:50 +0000

    init

--- database/saw.sql
+++ database/saw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Dez-2021 às 23:55
+-- Tempo de geração: 17-Dez-2021 às 14:34
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.12
 
@@ -24,32 +24,141 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `image` varchar(244) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `forgotpassword`
+--
+
+CREATE TABLE `forgotpassword` (
+  `id` int(11) NOT NULL,
+  `email` varchar(144) NOT NULL,
+  `token` varchar(256) NOT NULL,
+  `data` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `type` varchar(144) NOT NULL,
+  `message` longtext NOT NULL,
+  `error` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(144) NOT NULL,
+  `price` double NOT NULL,
+  `data` datetime NOT NULL,
+  `description` longtext NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `productimage`
+--
+
+CREATE TABLE `productimage` (
+  `id` int(11) NOT NULL,
+  `name` varchar(144) NOT NULL,
+  `type` varchar(144) NOT NULL,
+  `data` blob NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `studentCode` int(7) NOT NULL,
-  `firstName` varchar(144) CHARACTER SET utf8 NOT NULL,
-  `lastName` varchar(144) CHARACTER SET utf8 NOT NULL,
-  `gender` varchar(144) CHARACTER SET utf8 NOT NULL,
-  `telephone` int(9) NOT NULL,
-  `course` varchar(144) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(244) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(256) CHARACTER SET utf8 NOT NULL,
+  `firstName` varchar(144) NOT NULL,
+  `lastName` varchar(144) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `location` varchar(144) NOT NULL,
+  `email` varchar(144) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `level` enum('Admin','User') NOT NULL DEFAULT 'User',
+  `status` enum('Blocked','Allowed') NOT NULL,
   `createdDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Extraindo dados da tabela `user`
+-- Estrutura da tabela `userimage`
 --
 
-INSERT INTO `user` (`id`, `studentCode`, `firstName`, `lastName`, `gender`, `telephone`, `course`, `email`, `password`, `createdDate`) VALUES
-(18, 8200615, 'Sérgio', 'Félix', 'M', 916275619, 'DWDM', 'sergio98@outlook.pt', '$2y$12$s5zHbubqAYjACPQuChg8bOZiOMj40BRw9FcLyszJf3/C7Cmz1syTa', '2021-12-07 20:52:25');
+CREATE TABLE `userimage` (
+  `id` int(11) NOT NULL,
+  `name` varchar(144) NOT NULL,
+  `type` varchar(144) NOT NULL,
+  `data` blob NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `forgotpassword`
+--
+ALTER TABLE `forgotpassword`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product_user1_idx` (`user_id`),
+  ADD KEY `fk_product_categories1_idx` (`categories_id`);
+
+--
+-- Índices para tabela `productimage`
+--
+ALTER TABLE `productimage`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_productImages_product_idx` (`product_id`);
 
 --
 -- Índices para tabela `user`
@@ -58,14 +167,80 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `userimage`
+--
+ALTER TABLE `userimage`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_userImage_user1_idx` (`user_id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `forgotpassword`
+--
+ALTER TABLE `forgotpassword`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `productimage`
+--
+ALTER TABLE `productimage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `userimage`
+--
+ALTER TABLE `userimage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `fk_product_categories1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_product_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `productimage`
+--
+ALTER TABLE `productimage`
+  ADD CONSTRAINT `fk_productImages_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `userimage`
+--
+ALTER TABLE `userimage`
+  ADD CONSTRAINT `fk_userImage_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
