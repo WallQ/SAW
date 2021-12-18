a71d6c5bc1f2a038a9738f69e249cda6b09695f3
Sérgio Félix <sergio98@outlook.pt>
2021-12-18 00:58:37 +0000

    init

--- database/saw.sql
+++ database/saw.sql
@@ -1,43 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 18-Dez-2021 às 00:05
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `saw`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `category`
---
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `category` varchar(45) NOT NULL,
   `image` varchar(244) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `forgotpassword`
---
 
 CREATE TABLE `forgotpassword` (
   `id` int(11) NOT NULL,
@@ -46,22 +15,10 @@ CREATE TABLE `forgotpassword` (
   `data` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `gender`
---
-
 CREATE TABLE `gender` (
   `id` int(11) NOT NULL,
   `gender` enum('Masculine','Feminine','Other') NOT NULL DEFAULT 'Other'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `log`
---
 
 CREATE TABLE `log` (
   `id` int(11) NOT NULL,
@@ -69,12 +26,6 @@ CREATE TABLE `log` (
   `message` longtext NOT NULL,
   `error` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `product`
---
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
@@ -86,12 +37,6 @@ CREATE TABLE `product` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `productimage`
---
-
 CREATE TABLE `productimage` (
   `id` int(11) NOT NULL,
   `name` varchar(144) NOT NULL,
@@ -100,22 +45,10 @@ CREATE TABLE `productimage` (
   `product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `state`
---
-
 CREATE TABLE `state` (
   `id` int(11) NOT NULL,
   `state` varchar(144) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `user`
---
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -133,12 +66,6 @@ CREATE TABLE `user` (
   `gender_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `userimage`
---
-
 CREATE TABLE `userimage` (
   `id` int(11) NOT NULL,
   `name` varchar(144) NOT NULL,
@@ -147,159 +74,77 @@ CREATE TABLE `userimage` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `category`
---
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `forgotpassword`
---
 ALTER TABLE `forgotpassword`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `gender`
---
 ALTER TABLE `gender`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `log`
---
 ALTER TABLE `log`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `product`
---
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_product_category1_idx` (`category_id`),
   ADD KEY `fk_product_user1_idx` (`user_id`);
 
---
--- Índices para tabela `productimage`
---
 ALTER TABLE `productimage`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_productImage_product1_idx` (`product_id`);
 
---
--- Índices para tabela `state`
---
 ALTER TABLE `state`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_state1_idx` (`state_id`),
   ADD KEY `fk_user_gender1_idx` (`gender_id`);
 
---
--- Índices para tabela `userimage`
---
 ALTER TABLE `userimage`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_userImage_user1_idx` (`user_id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `category`
---
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `forgotpassword`
---
 ALTER TABLE `forgotpassword`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `gender`
---
 ALTER TABLE `gender`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `log`
---
 ALTER TABLE `log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `product`
---
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `productimage`
---
 ALTER TABLE `productimage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `state`
---
 ALTER TABLE `state`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `user`
---
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `userimage`
---
 ALTER TABLE `userimage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `product`
---
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_product_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_product_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Limitadores para a tabela `productimage`
---
 ALTER TABLE `productimage`
   ADD CONSTRAINT `fk_productImage_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Limitadores para a tabela `user`
---
 ALTER TABLE `user`
   ADD CONSTRAINT `fk_user_gender1` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_user_state1` FOREIGN KEY (`state_id`) REFERENCES `state` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Limitadores para a tabela `userimage`
---
 ALTER TABLE `userimage`
   ADD CONSTRAINT `fk_userImage_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
